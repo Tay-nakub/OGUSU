ad38e3b5e13efdf6f29a0eb0c3d47276a1f1fe59
XTH-TOO <XTH-TOO@XTH-TOO>
2017-06-03 11:04:41 +0700

    push -u origin master

--- StockControl/dbClss.cs
+++ StockControl/dbClss.cs
@@ -16,6 +16,9 @@ namespace StockControl
 {
     class dbClss
     {
+
+        public string ssss = "";
+        public string version = "";
         public static Telerik.WinControls.UI.RadRibbonForm CreateForm(string form)
         {
             try
